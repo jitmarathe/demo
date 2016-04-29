c1d6d44bac3423d5a87299146c8dcb9272a0d8c6
ASHISH TIWARI <ashish8mumbai@gmail.com>
2016-04-29 08:34:38 +0000

    Made changes to JpaBookingService file

--- src/main/java/org/springframework/samples/travel/JpaBookingService.java
+++ src/main/java/org/springframework/samples/travel/JpaBookingService.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
